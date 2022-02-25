8c2b93c18a24713fbb1dde86015103d6d8b956a4
softronaut <mathronaut@gmail.com>
2022-02-26 00:54:13 +0300

    DuoNotifier removed

--- lib/podex.dart
+++ lib/podex.dart
@@ -6,7 +6,6 @@ import 'dart:collection';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 part 'src/debounced.dart';
-part 'src/duo.dart';
 part 'src/replay.dart';
 part 'src/throttle.dart';
 
